fb36404cbf3e9ca4d79555abe8f3ec0403200690
Dennis Duffy <dennislduffy@gmail.com>
2021-04-12 20:57:32 -0500

    linked dimension variable from javascript to css

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 let container = document.querySelector(".container");
 
-for (i = 0; i < 16; i++) {
+let dimension = prompt("How many squares high would you like the grid? Please enter a number between 0 and 10: ")
+
+if(dimension > 10){
+
+  let dimension = 10
+
+}
+
+document.documentElement.style.setProperty("--dimension", dimension);
+
+for (i = 0; i < dimension * dimension; i++) {
 
   let etchContainer = document.createElement("div");
 
   etchContainer.classList.add("etch-div");
 
-  etchContainer.classList.add("color-new")
+  etchContainer.classList.add("color-new");
 
   container.appendChild(etchContainer);
 }
